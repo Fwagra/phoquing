bcd9a01917e73d295a752af4d261fcfce242a39e
Fabio <fcerqueira@datasolution.fr>
2017-12-12 18:08:16 +0100

    Translations

---
+++ resources/lang/en/tracks.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Tracks Language Lines
+    |--------------------------------------------------------------------------
+    |
+    |
+    */
+
+    'no_tracks_found' => 'No tracks found yet',
+    'loading_tracks' => 'Loading your tracks',
+    'tracks_title' => 'My tracks',
+    'stats_title' => 'Stats',
+    'add_new' => 'Add a track',
+    'add_button' => 'Add',
+    'date' => "Date",
+    'start' => "Start",
+    'end' => "End",
+    'comment' => "Comment",
+    'category' => "Category",
+    'cancel_edit_button' => 'Cancel',
+    'edit_task' => 'Edit a track'
+];
